e58cc8d3a2b97fa3a7ebbbed0d8c55dc647a55ad
Qw3rtyone <mathew.aswin@ymail.com>
2017-11-27 17:15:36 +0000

    TurnBased + Movement + Start Base (Harry + Aswin)
    Start of the turn based system and basic movement (Requires improvement) and Starting positions

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chimera
+{
+    internal class PlayerBehaviour : MonoBehaviour
+    {
+        public void Movement()
+        {
+            var x = Input.GetAxis("Horizontal")*10f;
+            var y = Input.GetAxis("Vertical")*10f;
+            
+            transform.Translate(x, y, 0);
+        }
+    }
+}
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -9,18 +9,51 @@ namespace Chimera
     {
 
         public GameObject[] Players;
-
+        public GameObject[] Pawns;
+        int index = 0;
         public void SetupPlayers()
         {
-            for (int x = -1; x < 19; x++)
+            
+            Pawns = new GameObject[5];
+            for (int x = -1; x < 21; x++)
             {
-
+                
+                if(CheckPlayerTile(x))
+                {
+                    GameObject Pawn = Instantiate(Players[index], new Vector3(x, -1, 0f), Quaternion.identity) as GameObject;
+                    Pawn.AddComponent<PlayerBehaviour>();
+                    Pawns[index] = Pawn;
+                    int a = Pawns.Length;
+                    Debug.Log("Length " + a);
+                    index++;
+                }
             }
+        }
+        bool CheckPlayerTile(int x)
+        {
+            if (x == 2 || x == 6 || x == 10 || x == 14 || x == 18)
+                return true;
+            else
+                return false;
         }
 
         // Update is called once per frame
-        void Update()
+        public void UpdatePlayers()
         {
+            if (index >= 5)
+                index = 0;
+                   
+            if(Input.GetKeyDown("space"))
+            {
+                index++;
+                Debug.Log("Spaace");
+
+            }
+            
+
+            GameObject p = Pawns[index];
+            Debug.Log(index);
+            p.GetComponent<PlayerBehaviour>().Movement();
 
         }
     }
