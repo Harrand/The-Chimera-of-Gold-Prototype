404ac83070403c90b0a375f77600fa1738763df3
ciaraob98 <ciara-obrien@hotmail.com>
2017-11-27 15:57:34 +0000

    Added simple dice functionality

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chimera
+{
+	public class Dice : MonoBehaviour
+	{		
+		public GameObject[] diceFace;
+
+		void Start()
+		{
+			Debug.Log(Dice.Roll(1,6));
+		}
+		
+		static int Roll (int min, int max) 
+		{
+			int rolled = Random.Range(min,max);
+			return rolled;
+		}
+
+		void Render (int numberRolled, int width, int height, int x, int y)
+		{		
+			for (int i = 0; i<numberRolled; i++)
+			{
+				Instantiate(diceFace);
+			}
+		}
+
+	}
+}
+
+
