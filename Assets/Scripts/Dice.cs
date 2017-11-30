86d50f00e4af2758ceac42e96deeece7a274d8d6
Harrand <theplumder@gmail.com>
2017-11-30 12:21:47 +0000

    Documented Dice.cs, added contribution comment and optimised imports.

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Chimera
 {
 	public class Dice : MonoBehaviour
 	{		
+        // Harry and Ciara 30/11/2017
 		public GameObject diceFace;
 		private Transform diceTransform;
 
@@ -13,6 +12,7 @@ namespace Chimera
 		{
             // right now const parameters, edit as needed
             const int width = 100, height = 100;
+            // upon script initialisation roll a default D6.
             this.Render(Dice.Roll(1, 6), 0, 0, width, height);
         }
 		
@@ -24,13 +24,15 @@ namespace Chimera
 
 		void Render (int numberRolled, int x, int y, int width, int height)
 		{
+            // Make copy of existing 'DiceFace' prefab
             diceFace = Instantiate(Resources.Load("Prefabs/DiceFace") as GameObject);
+            // Edit sprite to be loaded texture dice_faces/x.png where x is number rolled.
             diceFace.GetComponent<SpriteRenderer>().sprite = Resources.Load("dice_faces/" + numberRolled, typeof(Sprite)) as Sprite;
+            // White background or the whole thing is black.
             diceFace.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1);
+            // Set scale, position and rotation.
             diceFace.transform.localScale = new Vector3(width, height, 1);
             diceFace.transform.SetPositionAndRotation(new Vector3(x, y, 0), Quaternion.identity);
-            //Debug.Log("Rolled a " + numberRolled);
-            // TODO: Set Scale
 		}
 
 	}
