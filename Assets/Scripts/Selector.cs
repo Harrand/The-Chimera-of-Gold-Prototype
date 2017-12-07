7e91f757e4ac13622aa13261108846be80dd9ef4
Qw3rtyone <mathew.aswin@ymail.com>
2017-12-07 18:14:02 +0000

    Minor improvement on selector
    The circle now rotates, expands and contracts making it more visible

---
+++ Assets/Scripts/Selector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selector : MonoBehaviour {
+    Vector3 scale;
+	// Use this for initialization
+	void Start () {
+        scale = new Vector3(0,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(Vector3.forward, 10);
+        scale.x = Mathf.Sin(Time.time) /5;
+        scale.y = Mathf.Sin(Time.time) /5;
+        this.transform.localScale = scale;
+    }
+}
