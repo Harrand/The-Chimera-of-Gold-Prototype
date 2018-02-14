ac8e7669ca6b70d5ce2a9269f3f117c29e439f88
Bleach <zy18745@gmail.com>
2018-02-14 14:53:31 +0000

    Add a BFS_Find_Path function to DecisionTree class
    Yutian and Zibo's work

--- Assets/Scripts/DecisionTree.cs
+++ Assets/Scripts/DecisionTree.cs
@@ -9,6 +9,7 @@ namespace Chimera
 {
     public class DecisionTree
     {
+		int value;
 		public ObstacleManager obstaclescript;
 		public DecisionTree()
 		{
@@ -20,7 +21,8 @@ namespace Chimera
 			this.obstaclescript = obstaclescript;
 		}
 
-		public int BFS_Assese_Value(Vector2 startPosition)
+		//this method is to find all possibilities of paths after rolling a dice.
+		public List<Vector2> BFS_Find_Path(int moves,Vector2 startPosition)
 		{
 			// four direction: up,right,down,left 
 			int[,] dir = new int[4,2]
@@ -29,7 +31,87 @@ namespace Chimera
 				{0, -1}, {-1, 0}  
 			}; 
 
-			int value;
+			var path = new List<Vector2> ();
+			var visited = new HashSet<Vector2> (); //store node that has been visited
+
+			var waitList = new Queue<Vector2> ();  //store node that need to be visited
+			waitList.Enqueue (startPosition);
+
+			while (waitList.Count > 0 && moves > 0) 
+			{
+				Vector2 current = waitList.Dequeue ();
+				Vector2 neighbour = new Vector2 ();
+
+				Debug.Log("x position = " + current.x + "  y position = " + current.y);  //use to debug
+
+				visited.Add(current);  // add the current to the visited list
+
+				moves--;
+				for(int i = 0; i < 4; ++i){
+					neighbour.x = current.x+dir[i,0]; //search the neighbour node 
+					neighbour.y = current.y+dir[i,1];
+				
+					if ((isvalid(neighbour)) && !visited.Contains(neighbour))
+					{
+						if(ObstacleManager.CheckObstacle((int)neighbour.x,(int)neighbour.y))
+						{
+							if(moves!=0)
+							{
+								if (!path.Contains (current)) 
+								{
+									path.Add(current);
+								}
+								continue;
+							}
+							else if(moves == 0)
+							{
+								if (!path.Contains (current)) 
+								{
+									path.Add(current);
+								}
+								continue;
+							}
+							else
+							{
+								Debug.Log("moves less than 0!!!!");	
+							}
+						}
+						if (moves == 0) 
+						{
+							if (!path.Contains(neighbour)) 
+							{
+								path.Add(neighbour);
+							}
+							continue;
+						}
+						else
+						{
+							waitList.Enqueue(neighbour);  //join the neighbour to the waitList to wait for next search
+						}
+
+					}  
+				}
+
+				Debug.Log("we can't find the goal!!!!!!");
+
+
+
+
+			}
+			return path;
+
+		}
+
+		//this method is to decide which path should the AI to go. It compares the distance of goal position and the end position after moving.
+		public int BFS_Assese_Value(Vector2 startPosition)
+		{
+			// four direction: up,right,down,left 
+			int[,] dir = new int[4,2]
+			{  
+				{0, 1}, {1, 0},  
+				{0, -1}, {-1, 0}  
+			}; 
+				
 			Vector2 goalposition = new Vector2 (10, 18);
 
 			var visited = new HashSet<Vector2> (); //store node that has been visited
